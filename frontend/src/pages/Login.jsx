4a0a23a671972648b034016c5e5f79eb47fd9f03
Erick-Bravo <erickbrvo@gmail.com>
2023-02-22 00:31:16 -0800

    config vercel

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify"; 
-import { login, reset } from "../features/auth/authSlice";
+import { reset } from "../features/auth/authSlice";
 import Spinner from "../components/Spinner";
 
 const Login = () => {
