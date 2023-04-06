95ac74553010b7b2b13320b8f4ed8a00961c0210
Erick-Bravo <erickbrvo@gmail.com>
2023-04-05 22:00:36 -0700

    enum additions

--- frontend/src/enums.js
+++ frontend/src/enums.js
@@ -2,7 +2,11 @@ export const ModalType = {
     ADD_Partshouse: 'add-partshouse',
     ADD_Record: 'add-record',
     ADD_Part: 'add-part',
+    ADD_BuyURL: 'add-buyurl',
     DELETE_Partshouse: 'delete-partshouse',
     DELETE_Record: 'delete-record',
     DELETE_Part: 'delete-part',
+    EDIT_Partshouse: 'edit-partshouse',
+    EDIT_Record: 'edit-record',
+    EDIT_Part: 'edit-part',
 };
