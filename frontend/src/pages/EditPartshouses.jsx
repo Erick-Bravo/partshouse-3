0c93764b447cba83f8de9d8f520baf4e258ac0c6
Erick-Bravo <erickbrvo@gmail.com>
2023-04-17 13:05:04 -0700

    partshouse style update

--- frontend/src/pages/EditPartshouses.jsx
+++ frontend/src/pages/EditPartshouses.jsx
@@ -42,8 +42,8 @@ const EditPartshouses = () => {
 
   return (
     <NavInterface>
-      <Box bg={whitePaper} borderRadius="10px" p={["30px 30px"]} h="99%">
-        <Text fontStyle="italic">
+      <Box borderRadius="10px" p={["30px 30px"]} h="99%">
+        <Text fontStyle="italic" m="15px">
           Note: You will be unable to delete a Partshouse if it contains any
           Records
         </Text>
@@ -51,39 +51,60 @@ const EditPartshouses = () => {
         <ModalButton type={ModalType.ADD_Partshouse} text="Add a Partshouse" />
 
         {ph.length > 0 ? (
-          ph.map((p) => (
-            <Flex flexDir="column" color="black" p={["30px 0"]} key={p._id}>
-              <Text fontWeight="bold" fontSize={["30px"]} pb={["3"]}>
-                {p.name}
-              </Text>
-              <Text pb={["5"]}>
-                Number of Records: {p.length ? p.length : "No Records"}
-              </Text>
-
-              <Flex justifyContent="center">
-                <Button
-                  bg={whitePaper}
-                  mr={["9"]}
-                  _hover={{
-                    bg: "gray.200",
-                    cursor: "pointer",
-                  }}
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            flexDir="column"
+            color="black"
+            p={["30px 0"]}
+          >
+            {ph.map((p) => (
+              <Flex
+              flexDir="column"
+                key={p._id}
+                bg={whitePaper}
+                m="20px"
+                p="20px"
+                maxWidth={["600px"]}
+                width="100%"
+                borderRadius="10px"
+              >
+                <Text
+                  fontWeight="bold"
+                  fontSize={["20px", "30px"]}
+                  pb={["3"]}
+                  textAlign="center"
                 >
-                  Edit
-                </Button>
-                <Button
-                  bg={whitePaper}
-                  onClick={() => dispatch(deletePH(p._id))}
-                  _hover={{
-                    bg: "gray.200",
-                    cursor: "pointer",
-                  }}
-                >
-                  Delete
-                </Button>
+                  {p.name}
+                </Text>
+                <Text pb={["5"]}>
+                  Records: {p.length ? p.length : "No Records"}
+                </Text>
+                <Flex justifyContent="center" alignItems="center">
+                  <Button
+                    bg={whitePaper}
+                    mr={["9"]}
+                    _hover={{
+                      bg: "gray.200",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    bg={whitePaper}
+                    onClick={() => dispatch(deletePH(p._id))}
+                    _hover={{
+                      bg: "gray.200",
+                      cursor: "pointer",
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </Flex>
               </Flex>
-            </Flex>
-          ))
+            ))}
+          </Flex>
         ) : (
           <Text>You have not created any partshouses yet</Text>
         )}
