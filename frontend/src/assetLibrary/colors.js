d5aadc0c05fac0195c101aad8a4c9ee91bc62bfd
Erick-Bravo <erickbrvo@gmail.com>
2024-11-11 20:40:04 -0800

    old partshouse logo returns

--- frontend/src/assetLibrary/colors.js
+++ frontend/src/assetLibrary/colors.js
@@ -3,7 +3,7 @@
 export const blueWhale = "#213644";
 export const blueWhaleLight = "#305a75";
 export const blueWhaleLight_Hover = "#4c89b0";
-export const toupOrange = "#c6ab7c";
+export const toupOrange = "#fcdda7";
 export const whiteText = "whiteAlpha.900";
 export const whitePaper = "#f7f9fa";
 
--- frontend/src/components/Assets/Logo.jsx
+++ frontend/src/components/Assets/Logo.jsx
@@ -3,7 +3,7 @@ import { Flex, Image } from "@chakra-ui/react";
 const Logo = () => {
   return (
     <Flex pt={["30%", "30%", "20%", "0"]} w={["100%"]}>
-      <Image src="./PartshouseMain.svg" />
+      <Image src="./old-partshouse.png" />
     </Flex>
   );
 };
