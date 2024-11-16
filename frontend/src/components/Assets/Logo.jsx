4d685cef0729b64d704d0615dc0cab6d747c3bd2
Erick-Bravo <erickbrvo@gmail.com>
2024-11-16 07:07:23 -0800

    logo aspect fixed on desktop

--- frontend/src/components/Assets/Logo.jsx
+++ frontend/src/components/Assets/Logo.jsx
@@ -2,8 +2,8 @@ import { Flex, Image } from "@chakra-ui/react";
 
 const Logo = () => {
   return (
-    <Flex pt={["30%", "30%", "20%", "0"]} w={["100%"]}>
-      <Image src="./old-partshouse.png" />
+    <Flex pt={["30%", "30%", "20%", "0"]}>
+      <Image objectFit="contain" src="./old-partshouse.png" />
     </Flex>
   );
 };
